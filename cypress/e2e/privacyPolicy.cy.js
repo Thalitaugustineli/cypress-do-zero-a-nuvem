4f8cf3eb9cbe1dc18165d4ddacc7c4942d8352ba
Thalita Augustineli <tata_augustineli@hotmail.com>
2025-12-12 18:17:27 -0300

    Conclusão do curso

--- cypress/e2e/privacyPolicy.cy.js
+++ cypress/e2e/privacyPolicy.cy.js
@@ -1,4 +1,4 @@
- it.only('testa a página da política de privacidade de forma independente', ()=> {
+ it.skip('testa a página da política de privacidade de forma independente', ()=> {
     cy.visit('./src/privacy.html')
     cy.contains('h1', 'CAC TAT - Política de Privacidade')
       .should('be.visible')
@@ -6,3 +6,17 @@
     cy.contains('p', 'Talking About Testing')
       .should('be.visible')
   })
+
+  // Exércicio 13.1: lodash 
+
+  Cypress._.times(3, () => {
+    
+    it.only('Validar a página com o lodash', () => {
+       cy.visit('./src/privacy.html')
+    cy.contains('h1', 'CAC TAT - Política de Privacidade')
+      .should('be.visible')
+
+    cy.contains('p', 'Talking About Testing')
+      .should('be.visible')
+      })
+    })
